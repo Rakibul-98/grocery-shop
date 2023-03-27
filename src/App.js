af50e8763a19b97bbd32c213a85afb5edff8ea66
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-03-27 11:37:27 +0600

    router added successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home/Components/Home/Home';
+import { router } from './Routes/Routes';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
